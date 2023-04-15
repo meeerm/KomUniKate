39d14a3740b53e7161585eefd8975c65ca11c96d
Timo Alejandro Aranjo <alex.aranjo@gmail.com>
2023-04-15 03:19:51 -0500

    Added REST API portion (untested code)

--- api.py
+++ api.py
@@ -1,4 +1,8 @@
 import mysql.connector
+from flask import Flask
+
+#Create a flask application
+app = Flask(__name__)
 
 # Establish a connection to the database
 db = mysql.connector.connect(
@@ -11,6 +15,52 @@ db = mysql.connector.connect(
 
 
 )
+
+
+#This takes an array and converts it into a format that we expect from our get request
+#Every comment should have no newlines in it but each comment itself should be separated from each other using
+#newlines
+def convert_Array_To_Return_Body(array):
+    #Declares the string we will return
+    string = ""
+   
+    #For each comment, append it an a newline to the end
+    for item in array:
+        string += array + "\n"
+   
+    return string
+
+#When we get a 'GET' request on the url ending '/api/retrieve', we do this
+#This is what gets us a list of comments stored on the server
+@app.route('/api/retrieve', methods=['GET'])
+def retrieve():
+    #We declare the comment array that we will be returning
+    comment_Array = None
+    #We grab the index and length 'GET' variables from the HTTP packet
+    index = int(request.args.get('index'))
+    length = int(request.args.get('length'))
+    try:
+        #This will fail if the index is out of bounds
+        #We need to account for that
+        comment_Array = get_comments(index, length)
+    except:
+        #Return that it was a bad request if the index was out of bounds
+        return 'A 400 (Bad Request) error has occurred. This is probably an error having to do with the index        chosen', 400
+   
+    #Convert the array to our format and return it along with an http response code of 200
+    return convert_Array_To_Return_Body(comment_Array), 200
+   
+
+#This is called whenever a 'POST' request is made to the url ending '/api/post'
+#Basically a request to make a new comment
+@app.route('/api/post', methods=['POST'])
+def post():
+    #We take the body of the http packet (the comment to be posted), and we add insert it
+    insert_comment(request.data)
+    #Indicate that the server sucessfully processed the data but won't return anything by using a 204
+    #http response code
+    return '', 204
+
 
 # Define a function to insert a new comment into the COMMENTS table
 def insert_comment(comment):
@@ -54,3 +104,7 @@ def get_comments(comment_number, length):
   results = cursor.fetchall()
   comments = [row[0] for row in result]
   return comments
+
+#Make the REST API server listen on all IP's and port 4000
+app.run(host='0.0.0.0', port=4000)
+
